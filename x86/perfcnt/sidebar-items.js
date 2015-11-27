97fa6ada6c605ff5980620d3247b629b37094888
Gerd Zellweger <mail@gerdzellweger.com>
2015-11-27 08:08:08 +0100

    Update documentation

--- x86/perfcnt/sidebar-items.js
+++ x86/perfcnt/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["available_counters","Return performance counter description for the running micro-architecture."]],"mod":[["intel",""]]});
+initSidebarItems({"fn":[["core_counters","Return all core performance counters for the running micro-architecture."],["uncore_counters","Return all uncore performance counters for the running micro-architecture."]],"mod":[["intel",""]]});
